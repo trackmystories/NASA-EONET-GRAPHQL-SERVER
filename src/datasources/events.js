32cd7154022160875d8661274395e67a1ea28507
A Shirazee <43073387+trackmystories@users.noreply.github.com>
2021-01-14 20:38:36 +0500

    commit

--- src/datasources/events.js
+++ src/datasources/events.js
@@ -7,19 +7,44 @@ class EventsAPI extends RESTDataSource {
     this.baseURL = 'https://eonet.sci.gsfc.nasa.gov/api/v3/';
   }
 
-  eventReducer(item){
-    return {
-
-    }
-  }
-
-
-
-  async getAllEvents(){
+  async queryAllEvents(){
     const response = await this.get('events');
     return response.events;
   }
 
+
+    async getAllEvents() {
+    const  response = await this.queryAllEvents();
+    return Array.isArray(response)
+    ? response.map((item) => this.eventReducer(item))
+    : []
+    }
+
+
+  eventReducer(item){
+    return {
+        id: item.id,
+        title: item.title,
+        discription: item.discription,
+        link: item.link,
+        closed: item.closed,
+        categories: item.categories,
+        sources :  item.sources.map(i => ({
+         id : i.id,
+         url: i.url,
+        })),
+        categories :  item.categories.map(i => ({
+         id : i.id,
+         title: i.title,
+        })),
+        geometry: item.geometry.map(i => ({
+         magnitudeValue : i.magnitudeValue,
+         magnitudeUnit: i.magnitudeUnit,
+         date: i.date,
+         type: i.type,
+        })),
+       }
+    }
 }
 
 module.exports = EventsAPI;
