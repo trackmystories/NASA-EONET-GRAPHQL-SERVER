ecc5bdb069a9a727c950b515b77c8dc5873b573d
A Shirazee <43073387+trackmystories@users.noreply.github.com>
2021-04-20 06:49:01 +0500

    Update events.js

--- src/datasources/events.js
+++ src/datasources/events.js
@@ -1,47 +1,43 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
-
+const {RESTDataSource} = require('apollo-datasource-rest');
 
 class EventsAPI extends RESTDataSource {
-  constructor(){
+  constructor() {
     super();
     this.baseURL = 'https://eonet.sci.gsfc.nasa.gov/api/v3/';
   }
 
-  async queryAllEvents(){
+  async queryAllEvents() {
     const response = await this.get('events');
     return response.events;
   }
 
   async getAllEvents() {
-    const  response = await this.queryAllEvents();
+    const response = await this.queryAllEvents();
     return Array.isArray(response)
-    ? response.map((item) => this.eventReducer(item))
-    : []
+      ? response.map(item => this.eventReducer(item))
+      : [];
   }
 
-  async getEventById({ eventId }){
-    const res = await this.get('events', {id: eventId});
-    return this.eventReducer(res[0]);
-  }
-
-
-
-
- eventReducer(item){
+  eventReducer(item) {
     return {
-        id: item.id || 0,
-        title: item.title,
-        link: item.link,
-        categories : item.categories.map(i => ({
-         id : i.id,
-         title: i.title,
-        })),
-        sources :  item.sources.map(i => ({
-         id : i.id,
-         url: i.url,
-        })),
-       }
-    }
+      id: item.id || 0,
+      title: item.title,
+      link: item.link,
+      categories: item.categories.map(i => ({
+        id: i.id,
+        title: i.title,
+      })),
+      sources: item.sources.map(i => ({
+        id: i.id,
+        url: i.url,
+      })),
+      geometry: item.geometry.map(i => ({
+        date: i.date,
+        type: i.type,
+        coordinates: i.coordinates,
+      })),
+    };
+  }
 }
 
 module.exports = EventsAPI;
